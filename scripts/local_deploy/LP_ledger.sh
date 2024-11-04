cbaf6d6cc1612397bbcf875bd47b8a386d3b552d
Harshit Chauhan <kcn9596@gmail.com>
2024-11-04 22:09:45 +0530

    fixed bug

--- scripts/local_deploy/LP_ledger.sh
+++ scripts/local_deploy/LP_ledger.sh
@@ -17,11 +17,11 @@ echo "Token Name: $TOKEN_NAME"
 echo "Token Symbol: $TOKEN_SYMBOL"
 
 # Set initial parameters
-export PRE_MINTED_TOKENS=10_000_000_000
+export PRE_MINTED_TOKENS=10_000_000_000_000_000_000
 export TRANSFER_FEE=10_000
 
 # Switch to the default identity and get its principal ID
-dfx identity use default
+dfx identity use Harshit
 export DEFAULT=$(dfx identity get-principal)
 echo "DEFAULT principal: $DEFAULT"
 
