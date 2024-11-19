d6458ad716d6dffae240e3162d27016eac355c05
nikhilQuadb <nikhil.rai@quadbtech.com>
2024-11-19 16:26:26 +0530

    fix bug

--- src/valueswap_frontend/src/components/landingPageComponents/Methods.jsx
+++ src/valueswap_frontend/src/components/landingPageComponents/Methods.jsx
@@ -15,8 +15,8 @@ const Methods = () => {
                     <img src={MethodsImageOne} alt="MethodImageOne" className='h-11/12  w-full mx-auto' />
                 </div>
 
-                <div className='w-1/2  py-[4.5rem] pl-20 flex flex-col rounded-r-lg items-start font-fahkwang text-4xl h-max bg-[#000211]'>
-                    <div className='mb-0 w-full '>
+                <div className='w-1/2   pl-20 flex flex-col rounded-r-lg items-start font-fahkwang text-4xl bg-[#000211]'>
+                    <div className='mb-0 w-full h-11/12'>
                         <div className='flex gap-2 '>
                             <span className=' '>{LandingPageData.MethodsText.UpperSection.HeadingWordOne}</span>
                             <GradientSpan>{LandingPageData.MethodsText.UpperSection.HeadingWordTwo}</GradientSpan>
