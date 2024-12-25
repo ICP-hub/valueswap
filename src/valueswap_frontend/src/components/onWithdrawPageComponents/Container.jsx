06680668969e9bc749f7b3990bdadd54ecfa8b53
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-12-25 17:00:06 +0530

    add Container component to manage liquidity withdrawal views and actions

--- src/valueswap_frontend/src/components/onWithdrawPageComponents/Container.jsx
+++ src/valueswap_frontend/src/components/onWithdrawPageComponents/Container.jsx
@@ -1,54 +1,71 @@
-import { useCallback, useState } from "react"
-import YourLiquidityPool from "./YourLiquidityPool"
-import GradientButton from "../../buttons/GradientButton"
-import WithdrawLiquidity from "./WithdrawLiquidity"
+import { useCallback, useMemo, useState } from "react";
+import YourLiquidityPool from "./YourLiquidityPool";
+import GradientButton from "../../buttons/GradientButton";
+import WithdrawLiquidity from "./WithdrawLiquidity";
+import TransactionComplete from "./TransactionComplete";
+import BorderGradientButton from "../../buttons/BorderGradientButton";
 
-const CURR_VIEW = [
-    {
-        title : "Your Liquidity Pool Balance",
-        component : <YourLiquidityPool/>
-    },
-    {
-        title : "Withdraw Liquidity",
-        component : <WithdrawLiquidity/>
-    },
-    {
-        title : null,
-        component : <></>
-    }
-]
+const Container = () => {
+    const [currView, setCurrView] = useState(0);
 
-const Container = ()=>{
-    const [currView, setCurrView] = useState(0)
+    const handleViewChange = () => {
+        setCurrView((prev) => (prev + 1) % CURR_VIEW.length);
+    };
 
-    const renderView = useCallback(()=>{
-        return(
+    const CURR_VIEW = useMemo(()=>[
+        {
+            title: "Your Liquidity Pool Balance",
+            component: <YourLiquidityPool />,
+            CTA: (
+                <GradientButton CustomCss="w-full" onClick={handleViewChange}>
+                    Withdraw
+                </GradientButton>
+            ),
+        },
+        {
+            title: "Withdraw Liquidity",
+            component: <WithdrawLiquidity />,
+            CTA: (
+                <GradientButton CustomCss="w-full" onClick={handleViewChange}>
+                    Confirm Withdraw
+                </GradientButton>
+            ),
+        },
+        {
+            title: null,
+            component: <TransactionComplete />,
+            CTA: (
+                <BorderGradientButton customCss={`bg-[#000711] z-10 !w-full`} onClick={()=>console.log("Close")}>
+                    Close
+                </BorderGradientButton>
+            ),
+        },
+    ]);
+
+    const renderView = useCallback(() => {
+        return (
             <>
                 {CURR_VIEW[currView].title && (
                     <>
-                    <h2 className="md:text-2xl font-fahkwang text-center">{CURR_VIEW[currView].title}</h2>
-                    <hr />
+                        <h2 className="md:text-2xl font-fahkwang text-center">{CURR_VIEW[currView].title}</h2>
+                        <hr />
                     </>
                 )}
                 {CURR_VIEW[currView].component}
             </>
-        )
-    },[currView])
+        );
+    }, [currView]);
 
-    const handleViewChange=()=>{
-        setCurrView((prev)=>((prev+1) % CURR_VIEW.length))
-    }
-
-    return(
-        <section className="md:min-w-[450px] min-w-[300px] aspect-square 
+    return (
+        <section
+            className="md:min-w-[450px] min-w-[300px] aspect-square 
         bg-[#05071D] rounded-xl border border-['rgba(255, 255, 255, 0.5)']] 
-        px-2 py-3 flex flex-col space-y-4">
+        px-2 py-3 flex flex-col space-y-4"
+        >
             {renderView()}
-            <GradientButton CustomCss="w-full" onClick={handleViewChange}>
-                {currView === 0 ? "Withdraw" : "Confirm Withdraw"}
-            </GradientButton>
+            {CURR_VIEW[currView].CTA}
         </section>
-    )
-}
+    );
+};
 
-export default Container
+export default Container;
