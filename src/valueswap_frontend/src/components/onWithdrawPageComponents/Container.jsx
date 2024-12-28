69208bc219616f511244ac32f79d2387a0e86e64
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-12-28 15:18:49 +0530

    update Container component for improved responsiveness and styling

--- src/valueswap_frontend/src/components/onWithdrawPageComponents/Container.jsx
+++ src/valueswap_frontend/src/components/onWithdrawPageComponents/Container.jsx
@@ -3,9 +3,9 @@ import Slider from './Slider';
 import TokenDisplay from './TokenDisplay';
 const Container = () => {
     return(
-        <div className="bg-transparent dividie-y divide-white divide-opacity-50
-        min-w-[500px] flex flex-col justify-center space-y-2">
-            <div className='backdrop-blur-[32px] md:p-6 rounded-lg border border-white border-opacity-75'>
+        <div className="bg-transparent dividie-y divide-white divide-opacity-50 p-2
+        md:min-w-[500px] sm:min-w-[450px] min-w-full flex flex-col md:justify-center justify-between space-y-2">
+            <div className='backdrop-blur-[32px] md:p-6 sm:p-4 p-2 rounded-lg border border-white border-opacity-75'>
                 <Slider/>
             </div>
             <TokenDisplay/>
