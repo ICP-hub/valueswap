5a2d7bf099c74286a446893f78e558b5a0b1e821
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-12-28 13:48:39 +0530

    refactor Container component to simplify view management and integrate Slider and TokenDisplay components

--- src/valueswap_frontend/src/components/onWithdrawPageComponents/Container.jsx
+++ src/valueswap_frontend/src/components/onWithdrawPageComponents/Container.jsx
@@ -1,71 +1,19 @@
-import { useCallback, useMemo, useState } from "react";
-import YourLiquidityPool from "./YourLiquidityPool";
-import GradientButton from "../../buttons/GradientButton";
-import WithdrawLiquidity from "./WithdrawLiquidity";
-import TransactionComplete from "./TransactionComplete";
-import BorderGradientButton from "../../buttons/BorderGradientButton";
-
+import GradientButton from '../../buttons/GradientButton';
+import Slider from './Slider';
+import TokenDisplay from './TokenDisplay';
 const Container = () => {
-    const [currView, setCurrView] = useState(0);
-
-    const handleViewChange = () => {
-        setCurrView((prev) => (prev + 1) % CURR_VIEW.length);
-    };
-
-    const CURR_VIEW = useMemo(()=>[
-        {
-            title: "Your Liquidity Pool Balance",
-            component: <YourLiquidityPool />,
-            CTA: (
-                <GradientButton CustomCss="w-full" onClick={handleViewChange}>
-                    Withdraw
-                </GradientButton>
-            ),
-        },
-        {
-            title: "Withdraw Liquidity",
-            component: <WithdrawLiquidity />,
-            CTA: (
-                <GradientButton CustomCss="w-full" onClick={handleViewChange}>
-                    Confirm Withdraw
-                </GradientButton>
-            ),
-        },
-        {
-            title: null,
-            component: <TransactionComplete />,
-            CTA: (
-                <BorderGradientButton customCss={`bg-[#000711] z-10 !w-full`} onClick={()=>console.log("Close")}>
-                    Close
-                </BorderGradientButton>
-            ),
-        },
-    ]);
-
-    const renderView = useCallback(() => {
-        return (
-            <>
-                {CURR_VIEW[currView].title && (
-                    <>
-                        <h2 className="md:text-2xl font-fahkwang text-center">{CURR_VIEW[currView].title}</h2>
-                        <hr />
-                    </>
-                )}
-                {CURR_VIEW[currView].component}
-            </>
-        );
-    }, [currView]);
-
-    return (
-        <section
-            className="md:min-w-[450px] min-w-[300px] aspect-square 
-        bg-[#05071D] rounded-xl border border-['rgba(255, 255, 255, 0.5)']] 
-        px-2 py-3 flex flex-col space-y-4"
-        >
-            {renderView()}
-            {CURR_VIEW[currView].CTA}
-        </section>
-    );
+    return(
+        <div className="bg-transparent dividie-y divide-white divide-opacity-50
+        min-w-[500px] flex flex-col justify-center space-y-2">
+            <div className='backdrop-blur-[32px] md:p-6 rounded-lg border border-white border-opacity-75'>
+                <Slider/>
+            </div>
+            <TokenDisplay/>
+            <GradientButton CustomCss="w-full">
+                Withdraw
+            </GradientButton>
+        </div>
+    )
 };
 
 export default Container;
