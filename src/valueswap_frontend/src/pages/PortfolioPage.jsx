3a05d18b31432478eeca1bf092375ffa199fa7c8
ArjunQBTech <arjun.sharma@quadbtech.com>
2025-01-04 12:26:02 +0000

    Update PortfolioPage layout to ensure minimum height and auto height adjustment

--- src/valueswap_frontend/src/pages/PortfolioPage.jsx
+++ src/valueswap_frontend/src/pages/PortfolioPage.jsx
@@ -8,7 +8,7 @@ const PortfolioPage = () => {
 
 
     return (
-        <div className='h-screen '>
+        <div className='min-h-screen h-auto'>
                 <div className='text-center mt-12'>
                 <span className='text-3xl leading-5'>Portfolio</span>
                 </div>
