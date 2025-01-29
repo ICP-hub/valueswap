60f5bff939c3fbe9bb3c3155f58b936108bd92be
ArjunQBTech <arjun.sharma@quadbtech.com>
2025-01-29 10:52:12 +0000

    Fixed Number issue and used pre_compute_swap

--- src/valueswap_frontend/src/Modals/Swap.jsx
+++ src/valueswap_frontend/src/Modals/Swap.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import { toast } from 'react-toastify'
 import { useAuth } from '../components/utils/useAuthClient'
 import { Principal } from '@dfinity/principal'
@@ -60,23 +60,23 @@ const Swap = () => {
   // Helper to fetch balance
 
   useEffect(() => {
-    const getSwapValue = async () => {
+    const fetchSwapValue = async () => {
       if (coinAmount) {
         const amount = parseFloat(coinAmount)
 
-        const swapValue = await backendActor.pre_compute_swap({
+        const precomputedSwap = await backendActor.pre_compute_swap({
           token1_name: payCoin.ShortForm,
           token_amount: amount,
           token2_name: receiveCoin.ShortForm,
           ledger_canister_id1: Principal.fromText(receiveCoin.CanisterId),
           ledger_canister_id2: Principal.fromText(receiveCoin.CanisterId)
         })
-        console.log('swapValue', swapValue)
-        setInitialSlipageAmount(swapValue[1])
-        // if (swapValue.length == 2) {
+        console.log('precomputedSwap', precomputedSwap)
+        setInitialSlipageAmount(precomputedSwap[1])
+        // if (precomputedSwap.length == 2) {
         //   setPoolNotFound(true)
         // }
-        setReceiveValue(swapValue[1])
+        setReceiveValue(precomputedSwap[1])
       } else {
         console.log('no coin Amount enter')
         setPoolNotFound(false)
@@ -90,7 +90,7 @@ const Swap = () => {
         // console.log("Balance", payCoinBalance);
       }
     }
-    getSwapValue()
+    fetchSwapValue()
   }, [payCoin, getBalance, coinAmount, receiveCoin])
 
   useEffect(() => {
@@ -311,7 +311,7 @@ const Swap = () => {
  const handleSettings = () => {
     setSettings((prev) => !prev)
  }
-
+ 
  const openModalWithSteps = async () => {
  
   setIsModalOpen(true);
@@ -390,7 +390,7 @@ const Swap = () => {
           <div className='flex justify-between'>
             <div className='text-4xl w-1/2 overflow-x-auto  scroll-smooth'>
               {coinAmount && payCoin && receiveCoin
-                ? Number(receiveValue)/100000000
+                ? (Number(receiveValue) / 100000000).toLocaleString('fullwide', { useGrouping: false })
                 : 0}
             </div>
             <BorderGradientButton customCss={`bg-gray-700 z-10`}>
