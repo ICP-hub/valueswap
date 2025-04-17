52f75d8e7e4b3d6166221635dd6d6af8b4a78282
nikquadbtech <nikhil.mahato@quadbtech.com>
2025-04-17 13:59:29 +0530

    handle faucet input for values 0 and less than 0

--- src/valueswap_frontend/src/buttons/GradientButton.jsx
+++ src/valueswap_frontend/src/buttons/GradientButton.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const GradientButton = ({ CustomCss, children, onClick }) => {
-
-    return (
-
-            <button className={` ${CustomCss} h-[45px]  button-gradient-wrapper mx-auto  text-white text-base font-gilroy rounded-lg py-4 px-12 sm:px-[70px]`} onClick={onClick}>
-                <span className="flex items-center justify-center p-1 button-gradient-content ">
-                    {children}
-                </span>
-            </button>
-
-
-    );
+const GradientButton = ({ CustomCss, children, onClick, disabled }) => {
+  return (
+    <button
+      className={` ${CustomCss} h-[45px]  button-gradient-wrapper mx-auto  text-white text-base font-gilroy rounded-lg py-4 px-12 sm:px-[70px]`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      <span className="flex items-center justify-center p-1 button-gradient-content ">
+        {children}
+      </span>
+    </button>
+  );
 };
 
-export default GradientButton;  
+export default GradientButton;
